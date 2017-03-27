5e534ab04f1bfb677940cef8cff5e1bf1a34f4b9
jonassjoh <jonassjoh@gmail.com>
2017-03-27 14:24:56 +0200

    Ey yooo

--- app/src/main/java/emilp/hallo/MainActivity.java
+++ app/src/main/java/emilp/hallo/MainActivity.java
@@ -1,17 +1,67 @@
 package emilp.hallo;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ((GlobalApplication) getApplication()).getSpotifyService().playSong("spotify:track:2MxcYeVuWGzFP7ybXcMRJH");
+        initActionbar();
+
+        //((GlobalApplication) getApplication()).getSpotifyService().playSong("spotify:track:2MxcYeVuWGzFP7ybXcMRJH");
+
+    }
+
+    private void initActionbar() {
+        ActionBar mActionBar = getSupportActionBar();
+        if (mActionBar != null) {
+
+            mActionBar.setDisplayShowHomeEnabled(false);
+            mActionBar.setDisplayShowTitleEnabled(false);
+            LayoutInflater mInflater = LayoutInflater.from(this);
+
+            View mCustomView = mInflater.inflate(R.layout.actionbar, null);
+
+            mActionBar.setDisplayHomeAsUpEnabled(false);
+            Toolbar toolbar = (Toolbar) mCustomView.findViewById(R.id.toolbar);
+            SearchView searchView = (SearchView) mCustomView.findViewById(R.id.toolbar_search);
+
+            DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+            ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_content_desc, R.string.close_content_desc) {
+
+                public void onDrawerClosed(View view)
+                {
+                    supportInvalidateOptionsMenu();
+                    //drawerOpened = false;
+                }
+
+                public void onDrawerOpened(View drawerView)
+                {
+                    supportInvalidateOptionsMenu();
+                    //drawerOpened = true;
+                }
+            };
+
+            mDrawerToggle.setDrawerIndicatorEnabled(true);
+            drawerLayout.setDrawerListener(mDrawerToggle);
+            mDrawerToggle.syncState();
+
+            mActionBar.setCustomView(mCustomView);
+            mActionBar.setDisplayShowCustomEnabled(true);
+        }
     }
 }
